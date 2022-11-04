623d43a7382f9617e1585ad6b2f58c2ab293fd25
1babo <1babo@proton.me>
2022-11-04 14:00:24 -0700

    STSW-STX query cleanup

--- defi-stsw/chart-stsw-stx.sql
+++ defi-stsw/chart-stsw-stx.sql
@@ -1,33 +1,34 @@
-with tokens as (
-    select contract, contract_id, power(10,(properties->>'decimals')::numeric) as factor
-    from token_properties
-    where contract_id in (
-        'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.wstx-token-v4a::null',
-        'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.stsw-token-v4a::stsw',
-        'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.lbtc-token-v1c::lbtc'
-    )
-), swaps as (
-    select * from transactions
-    where status = 1 and tx_type = 'contract call'
-	and contract_call_function_name like '%swap%'
-	and contract_call_contract_id like 'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.%swap%'
-	--and block_time > now() - interval '1 month'
+with tokens (token_x,base_x,token_y,base_y) as (VALUES
+    (null, 1e6 -- STX
+    ,'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.stsw-token-v4a::stsw', 1e6
+))
+
+, calls (contract_like,function_like) as (VALUES
+    ('SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.stackswap-swap-v%','swap-%-for-%'),
+    ('SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.stackswap-swap-router-%','router-swap')
 )
-select date_bin('24 hours', block_time, date_trunc('day',block_time)::timestamp) as interval,
-    max(fx.amount / fy.amount * tky.factor / tkx.factor ) as max_rate,
-    min(fx.amount / fy.amount * tky.factor / tkx.factor ) as min_rate,
-    avg(fx.amount / fy.amount * tky.factor / tkx.factor ) as avg_rate,
-    LEAST(0.1 + sum(fy.amount / tky.factor) / 20e6, 0.3) as lerp_vol,
-    log( avg(fx.amount / fy.amount * tky.factor / tkx.factor) ) * 0.3 + 0.42 as lerp_log
-	from swaps txs
-    join ft_events fy
-        on (txs.tx_id = fy.tx_id and sender_address in (fy.sender, fy.recipient)
-            and fy.asset_identifier = 'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.stsw-token-v4a::stsw')
-    join stx_events fx
-        on (txs.tx_id = fx.tx_id and sender_address in (fx.sender, fx.recipient)
-            )--and fx.asset_identifier = 'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.wstx-token-v4a::null')
-    join tokens tky
-        on (tky.contract_id = fy.asset_identifier)
-    join tokens tkx
-        on (tkx.contract_id = 'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.wstx-token-v4a::null')
-    group by interval
+
+, swaps as (
+    select tx_id, block_height, block_time, sender_address
+    from transactions
+    join calls on (
+        contract_call_contract_id like contract_like
+        and contract_call_function_name like function_like
+    )
+    where status = 1
+    -- and tx_type = 'contract call'
+	-- and block_time > now() - interval '1 month'
+)
+
+select date_bin('24 hours', block_time, date_trunc('month',now())) as interval
+, max(fx.amount / fy.amount * base_y / base_x ) as max_rate
+, min(fx.amount / fy.amount * base_y / base_x ) as min_rate
+, avg(fx.amount / fy.amount * base_y / base_x ) as avg_rate
+, LEAST(0.075 + sum(fy.amount / base_y) / 20e6, 0.3) as lerp_vol
+, log( avg(fx.amount / fy.amount * base_y / base_x) ) * 0.25 + 0.385 as lerp_log
+from swaps tx
+cross join tokens tk
+join ft_events fy on (fy.tx_id = tx.tx_id and tx.sender_address = any(array[fy.sender,fy.recipient])
+    and fy.asset_identifier = tk.token_y)
+join stx_events fx on (fx.tx_id = tx.tx_id and tx.sender_address = any(array[fx.sender,fx.recipient]))
+group by interval
