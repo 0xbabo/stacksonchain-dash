7f2229d2de906b7e33a7507bf8108ce6708305b0
1babo <1babo@proton.me>
2022-11-06 19:30:42 -0800

    Cleanup stray spaces in dapp catalog

--- etc/dapp-catalog-nftx.sql
+++ etc/dapp-catalog-nftx.sql
@@ -1,19 +1,19 @@
 -- NOTE: Does not track indirect contract calls by third party contracts.
 
 with cat_sc (link, name, contract_like, source_match) as (VALUES
-    ('https://price.btc.us/','price.btc.us', 'SP3XYJ8XFZYF7MD86QQ5EF8HBVHHZFFQ9HM6SPJNQ.%', ''),
-    ('https://gamma.io/collections/bns','Gamma BNS', '%.bns-%-%', 'SPNWZ5V2TPWGQGVDR6T7B6RQ4XMGZ4PXTEE0VQ0S.gamma-commission'),
-    ('https://gamma.io/','Gamma Market','SPNWZ5V2TPWGQGVDR6T7B6RQ4XMGZ4PXTEE0VQ0S.marketplace%', ''),
+    ('https://price.btc.us/','price.btc.us','SP3XYJ8XFZYF7MD86QQ5EF8HBVHHZFFQ9HM6SPJNQ.%',''),
+    ('https://gamma.io/collections/bns','Gamma BNS','%.bns-%-%','SPNWZ5V2TPWGQGVDR6T7B6RQ4XMGZ4PXTEE0VQ0S.gamma-commission'),
+    ('https://gamma.io/','Gamma Market','SPNWZ5V2TPWGQGVDR6T7B6RQ4XMGZ4PXTEE0VQ0S.marketplace%',''),
     ('https://gamma.io/','Gamma Market','SP1CSHTKVHMMQJ7PRQRFYW6SB4QAW6SR3XY2F81PA.stxnft-auctions%',''),
-    ('https://www.tradeport.xyz/','Byzantion Market', 'SP2KAF9RF86PVX3NEE27DFV1CQX0T4WGR41X3S45C.%wrapper%', ''),
-    ('https://www.tradeport.xyz/','Byzantion Market', 'SP2KAF9RF86PVX3NEE27DFV1CQX0T4WGR41X3S45C.%market%', ''),
-    ('https://www.tradeport.xyz/','Byzantion Market', 'SP1BX0P4MZ5A3A5JCH0E10YNS170QFR2VQ6TT4NRH.byzantion-market%', ''),
-    ('https://www.stacksart.com/','Stacks Art', 'SPJW1XE278YMCEYMXB8ZFGJMH8ZVAAEDP2S2PJYG.stacks-art-market%', ''),
-    ('https://www.stackspunks.com/','Stacks Punks', 'SPJW1XE278YMCEYMXB8ZFGJMH8ZVAAEDP2S2PJYG.stacks-punks-market', ''),
-    ('https://thisisnumberone.com/','This is #1', 'SP3QSAJQ4EA8WXEDSRRKMZZ29NH91VZ6C5X88FGZQ.thisisnumberone%', ''),
-    ('https://www.heylayer.com/','HeyLayer NFTs', 'SP248HH800501WYSG7Z2SS1ZWHQW1GGH85ME34NT2.%', ''),
-    ('https://boom.money/','Boom NFTs', 'SP497E7RX3233ATBS2AB9G4WTHB63X5PBSP5VGAQ.boom-nfts', ''),
-    ('https://www.megapont.com/','Megapont', 'SP3D6PV2ACBPEKYJTCMH7HEN02KP87QSP8KTEH335.%', '')
+    ('https://www.tradeport.xyz/','Byzantion Market','SP2KAF9RF86PVX3NEE27DFV1CQX0T4WGR41X3S45C.%wrapper%',''),
+    ('https://www.tradeport.xyz/','Byzantion Market','SP2KAF9RF86PVX3NEE27DFV1CQX0T4WGR41X3S45C.%market%',''),
+    ('https://www.tradeport.xyz/','Byzantion Market','SP1BX0P4MZ5A3A5JCH0E10YNS170QFR2VQ6TT4NRH.byzantion-market%',''),
+    ('https://www.stacksart.com/','Stacks Art','SPJW1XE278YMCEYMXB8ZFGJMH8ZVAAEDP2S2PJYG.stacks-art-market%',''),
+    ('https://www.stackspunks.com/','Stacks Punks','SPJW1XE278YMCEYMXB8ZFGJMH8ZVAAEDP2S2PJYG.stacks-punks-market',''),
+    ('https://thisisnumberone.com/','This is #1','SP3QSAJQ4EA8WXEDSRRKMZZ29NH91VZ6C5X88FGZQ.thisisnumberone%',''),
+    ('https://www.heylayer.com/','HeyLayer NFTs','SP248HH800501WYSG7Z2SS1ZWHQW1GGH85ME34NT2.%',''),
+    ('https://boom.money/','Boom NFTs','SP497E7RX3233ATBS2AB9G4WTHB63X5PBSP5VGAQ.boom-nfts',''),
+    ('https://www.megapont.com/','Megapont','SP3D6PV2ACBPEKYJTCMH7HEN02KP87QSP8KTEH335.%','')
 )
 
 , cat_comm (link, name, comm_like) as (VALUES
