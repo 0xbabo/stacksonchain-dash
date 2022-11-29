0a55ff27e39686fd49e45e92f961b26bc765792f
1babo <1babo@proton.me>
2022-11-28 18:44:30 -0800

    Add missing entries for misc dapps

--- etc/dapp-catalog-other.sql
+++ etc/dapp-catalog-other.sql
@@ -1,11 +1,16 @@
 with links ("Link","Name","Category") as (VALUES
-    ('https://deeplake.finance/', 'Deep Lake', 'Testnet, Derivatives'),
-    ('https://liquidium.fi/', 'Liquidium', 'Testnet, NFT Lending'),
-    ('https://www.bitflow.finance/', 'Bitflow', 'Testnet, Stable Swap'),
-    ('https://www.magic.fun/', 'Magic Bridge', 'Testnet, BTC Bridge'),
+    ('https://wallet.hiro.so/', 'Hiro Wallet', 'Wallet, Extension'),
+    ('https://www.xverse.app/', 'Xverse', 'Wallet, Mobile'),
     ('https://www.sigle.io/', 'Sigle', 'Community, Publishing'),
-    ('https://www.console.xyz/', 'Console', 'Community, Chat, DAO'),
-    ('https://pravica.io/', 'Pravica', 'Community, Chat, DAO'),
-    ('https://stackerdaos.com/', 'Stacker DAOs', 'Community, DAO')
+    ('https://www.console.xyz/', 'Console', 'Community, DAO, Chat'),
+    ('https://pravica.io/', 'Pravica', 'Community, DAO, Chat'),
+    ('https://stackerdaos.com/', 'Stacker DAOs', 'Community, DAO'),
+    ('https://app.multisafe.xyz/', 'MultiSafe', 'Wallet, MultiSig'),
+    ('https://liquidium.fi/', 'Liquidium', 'Testnet, NFT Lending'),
+    ('https://www.zestprotocol.com/', 'Zest Protocol', 'Testnet, BTC Lending'),
+    ('https://deeplake.finance/', 'Deep Lake', 'Testnet, Derivatives'),
+    ('https://www.bitflow.finance/', 'Bitflow', 'Testnet, Stable Swap'),
+    ('https://www.dlc.link/', 'DLC.Link', 'Testnet, BTC Integration'),
+    ('https://www.magic.fun/', 'Magic Bridge', 'Testnet, BTC Bridge')
 )
 select * from links
