ca033ddddc6f7d558f397246c11cd3eed4935b1b
1babo <1babo@proton.me>
2022-12-11 17:45:02 -0800

    Update token holder queries

--- defi-alex/holders-atalex.sql
+++ defi-alex/holders-atalex.sql
@@ -2,52 +2,32 @@ with const as ( select
     'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.auto-alex::auto-alex' as the_asset
 )
 
-, props as (
-    select contract_id,
-        (properties ->> 'name') name,
-        (properties ->> 'symbol') symbol,
-        (properties ->> 'decimals') :: numeric decimals,
-        power(10, (properties ->> 'decimals') :: numeric) base
-    from token_properties
-    -- where contract_id in (...)
-)
-
 , events as (
-  select
-    ft.asset_event_type,
-    ft.asset_identifier as token,
-    ft.sender,
-    ft.recipient,
-    ft.amount / p.base as amount
-  from FT_EVENTS ft
-  join transactions tx using (tx_id)
-  join props p on (p.contract_id = ft.asset_identifier)
-  cross join const
-  where tx.status = 1 and asset_identifier = the_asset
+    select ft.sender, ft.recipient
+    , ft.amount / 1e8 as amount
+    from ft_events ft
+    cross join const
+    where asset_identifier = the_asset
 )
 
-, base as (
-    select
-      token as token,
-      recipient as address,
-      amount as amount
+, credit as (
+    select recipient as address
+    , sum(amount) as amount
     from events
-    where asset_event_type = 'mint' or asset_event_type = 'transfer'
-    
-    union all
-    
-    select
-      token as token,
-      sender as address,
-      -amount as amount
-    from events
-    where asset_event_type = 'burn' or asset_event_type = 'transfer'
+    group by 1
 )
 
-select
-  address,
-  sum(amount) as holding
-from base
-group by address
-order by holding desc
-limit 50
+, debit as (
+    select sender as address
+    , sum(amount) as amount
+    from events
+    group by 1
+)
+
+select address
+, credit.amount - coalesce(debit.amount,0) as balance
+from credit
+left join debit using (address)
+where address is not null
+order by balance desc nulls last
+limit 30
--- defi-stsw/holders-stsw.sql
+++ defi-stsw/holders-stsw.sql
@@ -2,52 +2,32 @@ with const as ( select
     'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.stsw-token-v4a::stsw' as the_asset
 )
 
-, props as (
-    select contract_id,
-        (properties ->> 'name') name,
-        (properties ->> 'symbol') symbol,
-        (properties ->> 'decimals') :: numeric decimals,
-        power(10, (properties ->> 'decimals') :: numeric) base
-    from token_properties
-    -- where contract_id in (...)
-)
-
 , events as (
-  select
-    ft.asset_event_type,
-    ft.asset_identifier as token,
-    ft.sender,
-    ft.recipient,
-    ft.amount / p.base as amount
-  from FT_EVENTS ft
-  join transactions tx using (tx_id)
-  join props p on (p.contract_id = ft.asset_identifier)
-  cross join const
-  where tx.status = 1 and asset_identifier = the_asset
+    select ft.sender, ft.recipient
+    , ft.amount / 1e6 as amount
+    from ft_events ft
+    cross join const
+    where asset_identifier = the_asset
 )
 
-, base as (
-    select
-      token as token,
-      recipient as address,
-      amount as amount
+, credit as (
+    select recipient as address
+    , sum(amount) as amount
     from events
-    where asset_event_type = 'mint' or asset_event_type = 'transfer'
-    
-    union all
-    
-    select
-      token as token,
-      sender as address,
-      -amount as amount
-    from events
-    where asset_event_type = 'burn' or asset_event_type = 'transfer'
+    group by 1
 )
 
-select
-  address,
-  sum(amount) as holding
-from base
-group by address
-order by holding desc
-limit 50
+, debit as (
+    select sender as address
+    , sum(amount) as amount
+    from events
+    group by 1
+)
+
+select address
+, credit.amount - coalesce(debit.amount,0) as balance
+from credit
+left join debit using (address)
+where address is not null
+order by balance desc nulls last
+limit 30
