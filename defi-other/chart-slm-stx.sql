00ca0b8481a48543e62035647305fa3bd635c3b1
1babo <1babo@proton.me>
2023-01-27 13:31:04 -0800

    Simplify BTC Monkeys token charts

--- defi-other/chart-slm-stx.sql
+++ defi-other/chart-slm-stx.sql
@@ -1,33 +1,17 @@
-with tokens as (
-    select contract, contract_id, power(10,(properties->>'decimals')::numeric) as factor
-    from token_properties
-    where contract_id in (
-        'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.wstx-token-v4a::null',
-        'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.age000-governance-token::alex',
-        'SP2KAF9RF86PVX3NEE27DFV1CQX0T4WGR41X3S45C.btc-monkeys-bananas::BANANA',
-        'SP125J1ADVYWGWB9NQRCVGKYAG73R17ZNMV17XEJ7.slime-token::SLIME'
-    )
-)
-
-select date_bin('12 hours', block_time, '2022-01-01') as interval
-, max(fx.amount / fy.amount * tky.factor / tkx.factor) as max_rate
-, min(fx.amount / fy.amount * tky.factor / tkx.factor) as min_rate
-, avg(fx.amount / fy.amount * tky.factor / tkx.factor) as avg_rate
-, LEAST(0.0 + sum(fy.amount / tky.factor) / 500e3, 1.0) as lerp_vol
-, log( avg(fx.amount / fy.amount * tky.factor / tkx.factor) ) * 0.25 + 0.5 as lerp_log
-from transactions txs
+select date_bin('1 day', block_time, '2022-01-01')::date as interval
+, max(fx.amount / fy.amount * 1) as max_rate
+, min(fx.amount / fy.amount * 1) as min_rate
+, avg(fx.amount / fy.amount * 1) as avg_rate
+, LEAST(1.0, 0 + sum(fy.amount)/1e6 / 500e3) as lerp_vol
+, log( avg(fx.amount / fy.amount * 1) ) * 0.20 + 0.50 as lerp_log
+from transactions tx
 join ft_events fy
-    on (txs.tx_id = fy.tx_id and sender_address in (fy.sender, fy.recipient)
-        and fy.asset_identifier = 'SP125J1ADVYWGWB9NQRCVGKYAG73R17ZNMV17XEJ7.slime-token::SLIME')
+    on (tx.tx_id = fy.tx_id and sender_address in (fy.sender, fy.recipient))
 join stx_events fx
-    on (txs.tx_id = fx.tx_id and sender_address in (fx.sender, fx.recipient)
-        )--and fx.asset_identifier = 'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.age000-governance-token::alex')
-join tokens tky
-    on (tky.contract_id = fy.asset_identifier)
-join tokens tkx
-    on (tkx.contract_id = 'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.wstx-token-v4a::null')
-where status = 1 and tx_type = 'contract call'
+    on (tx.tx_id = fx.tx_id and sender_address in (fx.sender, fx.recipient))
+where fy.asset_identifier = 'SP125J1ADVYWGWB9NQRCVGKYAG73R17ZNMV17XEJ7.slime-token::SLIME'
+-- and fx.asset_identifier = 'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.age000-governance-token::alex'
 and contract_call_function_name = 'swap-helper'
-and contract_call_contract_id like 'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.swap%'
+and contract_call_contract_id like 'SP3K8BC%0KBR9.swap%'
 --and block_time > now() - interval '1 month'
 group by interval
