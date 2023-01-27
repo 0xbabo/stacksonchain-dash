43b808b9e11536014d39bd2b2257cd7ea032205f
1babo <1babo@proton.me>
2023-01-27 13:48:22 -0800

    Update defi about panels

--- defi-diko/about-diko.sql
+++ defi-diko/about-diko.sql
@@ -1,7 +1,8 @@
-with links ("Description", "Link") as (VALUES
-    ('Website', 'https://arkadiko.finance/'),
-    ('Whitepaper', 'https://arkadiko.finance/arkadiko-whitepaper.pdf'),
-    ('Documentation', 'https://docs.arkadiko.finance/'),
-    ('More', 'https://owl.link/1babo.btc')
+with links ("Description", "Link") as (
+VALUES ('Website', 'https://arkadiko.finance/')
+-- , ('Whitepaper', 'https://arkadiko.finance/arkadiko-whitepaper.pdf')
+, ('Documentation', 'https://docs.arkadiko.finance/')
+, ('Analytics', 'https://info.arkadiko.finance/')
+, ('More', 'https://owl.link/1babo.btc')
 )
 select * from links
--- defi-stsw/about-stsw.sql
+++ defi-stsw/about-stsw.sql
@@ -1,9 +1,9 @@
-with links ("Description", "Link") as (VALUES
-    ('Website', 'https://app.stackswap.org/v2'),
-    ('Documentation', 'https://docs.stackswap.org/'),
-    -- ('Also Documentation', 'https://stackswap-official.gitbook.io/'),
-    -- ('Also Documentation?', 'https://stackswap.gitbook.io/'),
-    -- ('Whitepaper', 'https://stackswap-official.gitbook.io/stackswap/whitepaper'),
-    ('More', 'https://owl.link/1babo.btc')
+with links ("Description", "Link") as (
+VALUES ('Website', 'https://app.stackswap.org/v2')
+, ('Documentation', 'https://docs.stackswap.org/')
+-- , ('Also Documentation', 'https://stackswap-official.gitbook.io/')
+-- , ('Also Documentation?', 'https://stackswap.gitbook.io/')
+-- , ('Whitepaper', 'https://stackswap-official.gitbook.io/stackswap/whitepaper')
+, ('More', 'https://owl.link/1babo.btc')
 )
 select * from links
