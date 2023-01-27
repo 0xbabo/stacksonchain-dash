2a70d32b3dff82bdb1343768ed33271157b88a0b
1babo <1babo@proton.me>
2023-01-27 14:05:23 -0800

    Add catalog name to about meta query

--- meta/about.sql
+++ meta/about.sql
@@ -1,2 +1,4 @@
-SELECT version() as "DB Version"
-, pg_size_pretty( pg_database_size('stacks_blockchain_api') ) as "DB size"
+SELECT catalog_name
+, pg_size_pretty( pg_database_size('stacks_blockchain_api') ) as pg_db_size
+, version() as version
+from information_schema.information_schema_catalog_name
