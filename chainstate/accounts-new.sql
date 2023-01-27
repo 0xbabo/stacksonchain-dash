68653d6de1e00ae443627513cf11ca4bc2e8f23c
1babo <1babo@proton.me>
2023-01-27 14:51:37 -0800

    Count miner rewards for new accounts

--- chainstate/accounts-new.sql
+++ chainstate/accounts-new.sql
@@ -1,23 +1,23 @@
-with accounts as (
-select distinct on (address) block_height, block_time, address from (
-    select distinct block_height, recipient as address
+with creation as (
+select distinct on (1) address, block_height, block_time from (
+    select distinct recipient as address, block_height
     from stx_events
-    -- order by 2, 1
 union all
-    select distinct block_height, recipient as address
+    select distinct recipient as address, block_height
     from ft_events
-    -- order by 2, 1
 union all
-    select distinct block_height, recipient as address
+    select distinct recipient as address, block_height
     from nft_events
-    -- order by 2, 1
-) sub
+union all
+    select distinct recipient as address, mature_block_height as block_height
+    from miner_rewards
+) x
 join blocks using (block_height)
-order by address, block_height
+order by 1, block_height asc
 )
 
 select date_bin('7 days', block_time, '2021-01-03')::date as block_time
 , count(*)
-from accounts
+from creation
 where block_height > 1
 group by 1
