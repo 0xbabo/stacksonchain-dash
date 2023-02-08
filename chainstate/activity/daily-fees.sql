3d051b21629b31d507c50d9e86c59bc5bf9084f1
1babo <1babo@proton.me>
2023-02-07 19:51:51 -0800

    CTE for price on daily activity

--- chainstate/activity/daily-fees.sql
+++ chainstate/activity/daily-fees.sql
@@ -3,6 +3,17 @@ select '2023-01-01'::timestamp as ts_end
 , '2021-01-03'::timestamp as ts_begin
 )
 
+, daily_price as (
+select btc.ts::date as ts
+, avg(stx.open + stx.high + stx.low + stx.close)/4 as stx_avg
+, avg(btc.open + btc.high + btc.low + btc.close)/4 as btc_avg
+from prices.btc_usd btc
+left join prices.stx_usd stx on (stx.ts::date = btc.ts::date)
+where stx.timeframe = 'DAY' and btc.timeframe = 'DAY'
+group by 1
+order by 1
+)
+
 , daily as (
 select block_time::date as ts
 , sum(fee_rate)/1e6 as value
@@ -19,16 +30,14 @@ order by 1
 select ts
 , avg(value) over (order by ts rows between 6 preceding and current row) as value
 from daily d
+order by 1
 )
 
-select date_bin('2 days', d.ts, '2021-01-03')::date as ts
+select date_bin('2 days', ts, '2021-01-03')::date as ts
 , avg(d.value) as "Fees (STX)"
-, avg(d.value * (stx.open + stx.high + stx.low + stx.close)/4) as "Fees (USD value)"
-, avg(d.value * (stx.open + stx.high + stx.low + stx.close)/4
-    * 4/(btc.open + btc.high + btc.low + btc.close)) * 1e4 as "Fees (BTC value x1e4)"
+, avg(d.value * p.stx_avg) as "Fees (USD value)"
+, avg(d.value * p.stx_avg / p.btc_avg) * 1e4 as "Fees (BTC value x1e4)"
 from daily_running d
-left join prices.stx_usd stx on (stx.ts::date = d.ts)
-left join prices.btc_usd btc on (btc.ts::date = d.ts)
-where stx.timeframe = 'DAY' and btc.timeframe = 'DAY'
+left join daily_price p using (ts)
 group by 1
 order by 1
