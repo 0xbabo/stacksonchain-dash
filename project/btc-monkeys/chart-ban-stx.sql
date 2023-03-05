210c1a70db749c2caa945a8d754c259b4a87f2c4
1babo <1babo@proton.me>
2023-03-04 17:55:09 -0800

    Update & decompress recent on BTCM price charts

--- project/btc-monkeys/chart-ban-stx.sql
+++ project/btc-monkeys/chart-ban-stx.sql
@@ -1,9 +1,12 @@
-select date_bin('1 day', block_time, '2022-01-01')::date as interval
+select to_char( date_bin(
+    CASE WHEN block_time > now() - interval '7 days' THEN interval '1 hours' ELSE interval '24 hours' END
+    , block_time, '2021-01-03'), 'YYYY-MM-DD"T"HH24"h"') as ts
 , max(fx.amount / fy.amount * 1) as max_rate
 , min(fx.amount / fy.amount * 1) as min_rate
 , avg(fx.amount / fy.amount * 1) as avg_rate
-, LEAST(4.0, 0.0 + sum(fy.amount)/1e6 / 30e3) as lerp_vol
-, log( avg(fx.amount / fy.amount * 1) ) * 1.0 + 1.2 as lerp_log
+, LEAST(4.0, 0.0 + sum(fy.amount)/1e6 / 7e4) as lerp_vol
+, 0 as zero
+, log( avg(fx.amount / fy.amount * 1) ) * 0.8 + 1.2 as lerp_log
 from transactions tx
 join ft_events fy
     on (tx.tx_id = fy.tx_id and sender_address in (fy.sender, fy.recipient))
@@ -12,6 +15,7 @@ join stx_events fx
 where fy.asset_identifier = 'SP2KAF9RF86PVX3NEE27DFV1CQX0T4WGR41X3S45C.btc-monkeys-bananas::BANANA'
 -- and fx.asset_identifier = 'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.age000-governance-token::alex')
 and contract_call_function_name = 'swap-helper'
-and contract_call_contract_id like 'SP3K8BC%0KBR9.swap%'
+and contract_call_contract_id like 'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.swap%'
 --and block_time > now() - interval '1 month'
-group by interval
+group by 1
+order by 1
--- project/btc-monkeys/chart-slm-stx.sql
+++ project/btc-monkeys/chart-slm-stx.sql
@@ -1,8 +1,11 @@
-select date_bin('1 day', block_time, '2022-01-01')::date as interval
+select to_char( date_bin(
+    CASE WHEN block_time > now() - interval '7 days' THEN interval '1 hours' ELSE interval '24 hours' END
+    , block_time, '2021-01-03'), 'YYYY-MM-DD"T"HH24"h"') as ts
 , max(fx.amount / fy.amount * 1) as max_rate
 , min(fx.amount / fy.amount * 1) as min_rate
 , avg(fx.amount / fy.amount * 1) as avg_rate
-, LEAST(1.0, 0 + sum(fy.amount)/1e6 / 500e3) as lerp_vol
+, LEAST(1.0, -0.1 + sum(fy.amount)/1e6 / 500e3) as lerp_vol
+, 0 as zero
 , log( avg(fx.amount / fy.amount * 1) ) * 0.20 + 0.50 as lerp_log
 from transactions tx
 join ft_events fy
@@ -12,6 +15,7 @@ join stx_events fx
 where fy.asset_identifier = 'SP125J1ADVYWGWB9NQRCVGKYAG73R17ZNMV17XEJ7.slime-token::SLIME'
 -- and fx.asset_identifier = 'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.age000-governance-token::alex'
 and contract_call_function_name = 'swap-helper'
-and contract_call_contract_id like 'SP3K8BC%0KBR9.swap%'
+and contract_call_contract_id like 'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.swap%'
 --and block_time > now() - interval '1 month'
-group by interval
+group by 1
+order by 1
