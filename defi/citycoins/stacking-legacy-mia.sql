4873a0be9d5c1e1f2bdd76cbec05081fb282faed
1babo <1babo@proton.me>
2023-03-04 16:49:06 -0800

    Much improved citycoins TVL charts

--- defi/citycoins/stacking-legacy-mia.sql
+++ defi/citycoins/stacking-legacy-mia.sql
@@ -1,6 +1,10 @@
-with contracts (contract_id,start_block,final_cycle,token_base) as (
-VALUES('SP466FNC0P7JWTNM2R9T199QRZN1MYEDTAR0KP27.miamicoin-core-v1',24497,16,1e0)
-    , ('SP1H1733V5MZ3SZ9XRW9FKYGEZT0JDGEB8Y634C7R.miamicoin-core-v2',24497,34,1e6)
+with const as (
+select 24497 as start_block
+)
+
+, contracts (contract_id,final_cycle,token_base) as (
+VALUES('SP466FNC0P7JWTNM2R9T199QRZN1MYEDTAR0KP27.miamicoin-core-v1',16,1e0)
+    , ('SP1H1733V5MZ3SZ9XRW9FKYGEZT0JDGEB8Y634C7R.miamicoin-core-v2',34,1e6)
 )
 
 , lock_events as (
@@ -11,6 +15,7 @@ select tx_id
 , LEAST(arg_lock.period, final_cycle - arg_lock.start + 1) as lock_period
 , arg_tokens.amount/token_base as locked_amount
 from transactions tx
+cross join const
 join contracts on (contract_call_contract_id = contract_id)
 join lateral (
     select right(repr,-1)::int as period
@@ -27,13 +32,43 @@ where contract_call_function_name = 'stack-tokens'
 and status = 1
 )
 
-select cycle
+, poxl_info as (
+select cycle_id
 -- , count(distinct tx_id) as txs
 -- , count(distinct sender_address) as stackers
-, sum(locked_amount) as amount
+, sum(locked_amount) as stacked
 from lock_events lx
-join generate_series(1,50) cycle on (
-    lock_start <= cycle and cycle < lock_start + lock_period
+cross join const
+join generate_series(1,50) cycle_id on (
+    lock_start <= cycle_id and cycle_id < lock_start + lock_period
 )
 group by 1
 order by 1
+)
+
+, daily as (
+select token.ts::date as ts
+, avg(stx.open + stx.high + stx.low + stx.close)/4 as stx_avg
+, avg(token.open + token.high + token.low + token.close)/4 as token_avg
+from prices.mia_usd token
+left join prices.stx_usd stx on (stx.ts::date = token.ts::date)
+where token.timeframe = 'DAY'
+and stx.timeframe = 'DAY'
+group by 1
+)
+
+-- select cycle_id
+select block_height - (block_height - start_block) % 300 as block_height
+, stacked/1e2 as "TVL (100*MIA)"
+, stacked * coalesce(avg(pp.token_avg),0) as "TVL (USD value)"
+, stacked * coalesce(avg(pp.token_avg / pp.stx_avg),0) as "TVL (STX value)"
+, cycle_id
+from poxl_info
+cross join const
+join blocks bk on (cycle_id = (block_height - start_block) / 2100)
+left join daily pp on (pp.ts = block_time::date)
+-- where burn_block_height >= 668050
+-- and stacked > 0
+group by 1, stacked, cycle_id
+order by 1
+limit 500
--- defi/citycoins/stacking-legacy-nyc.sql
+++ defi/citycoins/stacking-legacy-nyc.sql
@@ -1,6 +1,10 @@
-with contracts (contract_id,start_block,final_cycle,token_base) as (
-VALUES('SP2H8PY27SEZ03MWRKS5XABZYQN17ETGQS3527SA5.newyorkcitycoin-core-v1',37449,10,1e0)
-    , ('SPSCWDV3RKV5ZRN1FQD84YE1NQFEDJ9R1F4DYQ11.newyorkcitycoin-core-v2',37449,28,1e6)
+with const as (
+select 37449 as start_block
+)
+
+, contracts (contract_id,final_cycle,token_base) as (
+VALUES('SP2H8PY27SEZ03MWRKS5XABZYQN17ETGQS3527SA5.newyorkcitycoin-core-v1',10,1e0)
+    , ('SPSCWDV3RKV5ZRN1FQD84YE1NQFEDJ9R1F4DYQ11.newyorkcitycoin-core-v2',28,1e6)
 )
 
 , lock_events as (
@@ -11,6 +15,7 @@ select tx_id
 , LEAST(arg_lock.period, final_cycle - arg_lock.start + 1) as lock_period
 , arg_tokens.amount/token_base as locked_amount
 from transactions tx
+cross join const
 join contracts on (contract_call_contract_id = contract_id)
 join lateral (
     select right(repr,-1)::int as period
@@ -27,13 +32,43 @@ where contract_call_function_name = 'stack-tokens'
 and status = 1
 )
 
-select cycle
+, poxl_info as (
+select cycle_id
 -- , count(distinct tx_id) as txs
 -- , count(distinct sender_address) as stackers
-, sum(locked_amount) as amount
+, sum(locked_amount) as stacked
 from lock_events lx
-join generate_series(1,50) cycle on (
-    lock_start <= cycle and cycle < lock_start + lock_period
+cross join const
+join generate_series(1,50) cycle_id on (
+    lock_start <= cycle_id and cycle_id < lock_start + lock_period
 )
 group by 1
 order by 1
+)
+
+, daily as (
+select token.ts::date as ts
+, avg(stx.open + stx.high + stx.low + stx.close)/4 as stx_avg
+, avg(token.open + token.high + token.low + token.close)/4 as token_avg
+from prices.nyc_usd token
+left join prices.stx_usd stx on (stx.ts::date = token.ts::date)
+where token.timeframe = 'DAY'
+and stx.timeframe = 'DAY'
+group by 1
+)
+
+-- select cycle_id
+select block_height - (block_height - start_block) % 300 as block_height
+, stacked/1e2 as "TVL (100*NYC)"
+, stacked * coalesce(avg(pp.token_avg),0) as "TVL (USD value)"
+, stacked * coalesce(avg(pp.token_avg / pp.stx_avg),0) as "TVL (STX value)"
+, cycle_id
+from poxl_info
+cross join const
+join blocks bk on (cycle_id = (block_height - start_block) / 2100)
+left join daily pp on (pp.ts = block_time::date)
+-- where burn_block_height >= 668050
+-- and stacked > 0
+group by 1, stacked, cycle_id
+order by 1
+limit 500
