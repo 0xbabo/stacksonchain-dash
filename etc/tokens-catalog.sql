28d4dc76627378c73d156f54d442063855ea42db
1babo <1babo@proton.me>
2023-03-04 22:09:30 -0800

    Improved token supply query for FT catalog

--- etc/tokens-catalog.sql
+++ etc/tokens-catalog.sql
@@ -10,46 +10,54 @@ select contract_id as asset_identifier
 , power(10, (properties ->> 'decimals') :: numeric) base
 from token_properties
 )
-, token_flow as (
-select account, asset_identifier, sum(amount) as balance from (
-    select recipient as account, asset_identifier, sum(amount) as amount
-    from ft_events
-    group by 1, 2
-union all
-    select sender as account, asset_identifier, -sum(amount) as amount
-    from ft_events
-    group by 1, 2
-) sub
-group by 1, 2
-)
 , token_supply as (
+with credit as (
+select fx.asset_identifier, recipient as address
+, sum(amount) as amount
+from tokens tk
+join ft_events fx using (asset_identifier)
+group by 1,2
+)
+, balance as (
+select cr.asset_identifier, cr.address
+, cr.amount - coalesce(debit.amount,0) as amount
+from credit cr
+left join lateral (
+    select sum(fx.amount) as amount from ft_events fx
+    where fx.asset_identifier = cr.asset_identifier
+    and fx.sender = cr.address
+) debit ON TRUE
+)
 select asset_identifier
 , split_part(asset_identifier,'.',2) as contract_name
-, sum(balance) as supply
 , count(*) as users
-from token_flow
+, count(*) filter (where amount > 0) as holders
+, sum(amount) as supply
+from balance
+join tokens using (asset_identifier)
 cross join const
-where account not in (stackswap_locker, arkadiko_locker) -- NOTE: BYPASSES CHECK FOR NULL ADDRESS (MINT/BURN)
-group by 1
+where address is not null
+and address not in (stackswap_locker, arkadiko_locker)
+group by 1,2
 )
 , token_prices as (
 select distinct on (1) token as asset_identifier, price
 from prices.dex_tokens_stx
 order by 1, ts desc
 )
-, token_pools as (
-select distinct on (contract_id, token_x, token_y) token_x, token_y, balance_x, balance_y
-from dex.swap_balances
-order by contract_id, token_x, token_y, block_height desc
-)
-, token_liquidity as (
-select tk.asset_identifier
-, sum(CASE WHEN tk.asset_identifier = token_x THEN balance_x ELSE balance_y END / tky.base * tk.base) as liquidity
-from tokens tk
-join token_pools on (tk.asset_identifier in (token_x, token_y))
-join tokens tky on (tky.asset_identifier = token_y) -- balances both use decimals from token_y
-group by tk.asset_identifier
-)
+-- , token_pools as (
+-- select distinct on (contract_id, token_x, token_y) token_x, token_y, balance_x, balance_y
+-- from dex.swap_balances
+-- order by contract_id, token_x, token_y, block_height desc
+-- )
+-- , token_liquidity as (
+-- select tk.asset_identifier
+-- , sum(CASE WHEN tk.asset_identifier = token_x THEN balance_x ELSE balance_y END / tky.base * tk.base) as liquidity
+-- from tokens tk
+-- join token_pools on (tk.asset_identifier in (token_x, token_y))
+-- join tokens tky on (tky.asset_identifier = token_y) -- balances both use decimals from token_y
+-- group by tk.asset_identifier
+-- )
 select contract_id as "Explorer"
 , block_height as genesis
 , CASE WHEN symbol is null THEN null ELSE format('%s (%s)', name, symbol) END as name_symbol
@@ -57,16 +65,16 @@ select contract_id as "Explorer"
     THEN left(contract_name,17)||'...'
     ELSE contract_name END as contract_name
 , decimals
-, users
+, users, holders
 , CASE WHEN base is null THEN supply::varchar
     ELSE to_char( supply / base, 'fm999G999G999G999G999G999D999')
     END as "Circulating Supply"
 , (supply / base * price) as "Market Cap (STX)"
-, (liquidity / base * price) as "Liquidity (STX)"
+-- , (liquidity / base * price) as "Liquidity (STX)"
 from tokens
 full join token_prices using (asset_identifier)
-full join token_liquidity using (asset_identifier)
+-- full join token_liquidity using (asset_identifier)
 full join token_supply using (asset_identifier)
 left join smart_contracts on (contract_id = split_part(asset_identifier,'::',1))
 where users > 2
-order by users desc nulls last
+order by holders desc nulls last, users desc nulls last
