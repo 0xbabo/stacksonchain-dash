df4a96adc7b6a39b00e44867d191f52d52d2d911
1babo <1babo@proton.me>
2023-03-04 22:05:41 -0800

    Cleanup for FARI chart

--- project/bitfari/chart-fari-stx.sql
+++ project/bitfari/chart-fari-stx.sql
@@ -1,22 +1,24 @@
 with tokens as (
-    select contract, contract_id, power(10,(properties->>'decimals')::numeric) as factor
-    from token_properties
-    where contract_id in (
-        'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.wstx-token-v4a::null',
-        'SP213KNHB5QD308TEESY1ZMX1BP8EZDPG4JWD0MEA.fari-token-mn::fari'
-    )
+select contract, contract_id, power(10,(properties->>'decimals')::numeric) as factor
+from token_properties
+where contract_id in (
+    'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.wstx-token-v4a::null',
+    'SP213KNHB5QD308TEESY1ZMX1BP8EZDPG4JWD0MEA.fari-token-mn::fari'
+)
 )
 
 , swaps as (
-    select * from transactions
-    where status = 1 and tx_type = 'contract call'
-	and contract_call_function_name like '%swap%'
-	and contract_call_contract_id like 'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.%swap%'
-    --and (block_time < '2022-03-14T00:00:00Z' or block_time > '2022-03-15T00:00:00Z') -- spurious events?
-	--and fx.amount / fy.amount < 50 -- remove spurious trades
+select * from transactions
+where status = 1 and tx_type = 'contract call'
+and contract_call_function_name like '%swap%'
+and contract_call_contract_id like 'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.%swap%'
+-- and (block_time < '2022-03-14T00:00:00Z' or block_time > '2022-03-15T00:00:00Z') -- spurious events?
+-- and fx.amount / fy.amount < 50 -- remove spurious trades
 )
 
-select date_bin('12 hours', block_time, '2022-01-01') as interval
+select to_char( date_bin(
+    CASE WHEN block_time > now() - interval '7 days' THEN interval '1 hours' ELSE interval '24 hours' END
+    , block_time, '2021-01-03'), 'YYYY-MM-DD"T"HH24"h"') as ts
 , max(fx.amount / fy.amount * tky.factor / 10^6 ) as max_rate
 , min(fx.amount / fy.amount * tky.factor / 10^6 ) as min_rate
 , avg(fx.amount / fy.amount * tky.factor / 10^6 ) as avg_rate
@@ -34,4 +36,4 @@ join tokens tky
     on (tky.contract_id = fy.asset_identifier)
 join tokens tkx
     on (tkx.contract_id = 'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.wstx-token-v4a::null')
-group by interval
+group by 1
